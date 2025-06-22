b7dbefc07c382702162230de91e13bd61b5542d6
drake <drake>
2025-06-22 11:52:02 -0400

    Movie hub doesnt exist just yet

--- TestBlazor/Program.cs
+++ TestBlazor/Program.cs
@@ -41,7 +41,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 app.UseResponseCompression();
-app.MapHub<MovieHub>("/moviehub");
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
